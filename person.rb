7948cb5cab5d547190bf977bc8dfa07542c38ce5
codehass <hassan.elouardy06@gmail.com>
2023-03-06 17:12:38 +0100

    Add person class

---
+++ person.rb
@@ -0,0 +1,21 @@
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    age.of_age? || parent_permission
+  end
+
+  private
+
+  def of_age?
+    age >= 18
+  end
+end
