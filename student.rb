2b17a4eeef094e40803935341cfeb732b25a7ccb
codehass <hassan.elouardy06@gmail.com>
2023-03-06 17:12:59 +0100

    Add student class

---
+++ student.rb
@@ -0,0 +1,10 @@
+class Student < Person
+  def initialize(age, classroom, _name = 'Unknown', _parent_permission: true)
+    super(age, _name = 'Unknown', parent_permission: true)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
+end
