8ad7405e72dd0d984970a56b20a858163d1901ee
codehass <hassan.elouardy06@gmail.com>
2023-03-16 09:41:54 +0100

    Fix errors

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -3,12 +3,12 @@ require_relative '../teacher'
 describe Teacher do
   describe '#new' do
     it 'Creates an instance of Teacher' do
-      teacher = Teacher.new(50, 'Jack', specialization: "math")
+      teacher = Teacher.new(50, 'Jack', specialization: 'math')
       expect(teacher).to be_instance_of Teacher
     end
 
     it 'Check  of the can_use_services? method ' do
-      teacher = Teacher.new(50, 'Jack', specialization: "math")
+      teacher = Teacher.new(50, 'Jack', specialization: 'math')
       expect(teacher.can_use_services?).to eq(true)
     end
   end
